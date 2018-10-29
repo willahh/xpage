43f0c926cc01f396d8cf4cdc16a0fe2448bd413b
William Ravel <wravel@gmail.com>
2018-10-30 00:47:08 +0100

    Update project.clj

--- project.clj
+++ project.clj
@@ -14,15 +14,14 @@
                  [cljs-ajax "0.7.4"]]
   
   :source-paths ["src"]
-
-  :profiles {:uberjar {:aot :all}}
   
   :aliases {"fig"       ["trampoline" "run" "-m" "figwheel.main"]
             "fig:build" ["trampoline" "run" "-m" "figwheel.main" "-b" "dev" "-r"]
             "fig:min"   ["run" "-m" "figwheel.main" "-O" "advanced" "-bo" "dev"]
             "fig:test"  ["run" "-m" "figwheel.main" "-co" "test.cljs.edn" "-m" xpage.test-runner]}
   
-  :profiles {:dev {:dependencies [[com.bhauman/figwheel-main "0.1.9"]
+  :profiles {:uberjar {:aot :all}
+             :dev {:dependencies [[com.bhauman/figwheel-main "0.1.9"]
                                   [com.bhauman/rebel-readline-cljs "0.1.4"]]
                    }})
 
